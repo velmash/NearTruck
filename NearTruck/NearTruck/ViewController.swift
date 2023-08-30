9b320b68de93692b74529963a87d2629b87b8767
윤형찬 <72960152+velmash@users.noreply.github.com>
2023-08-30 13:10:35 +0900

    :tada: 니어트럭 프로젝트 생성

---
+++ NearTruck/NearTruck/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NearTruck
+//
+//  Created by 윤형찬 on 2023/08/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
