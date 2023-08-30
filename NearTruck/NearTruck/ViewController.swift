31c7feb5903a86d5c2445a5f107eb685a7965af0
윤형찬 <72960152+velmash@users.noreply.github.com>
2023-08-30 15:13:17 +0900

    :hammer: firebase realtime db 연동(CRUD)

--- NearTruck/NearTruck/ViewController.swift
+++ NearTruck/NearTruck/ViewController.swift
@@ -14,12 +14,20 @@ class ViewController: UIViewController {
         testAPICall()
     }
     
+    @IBAction func read(_ sender: Any) {
+        readMen()
+    }
+    
+    @IBAction func write(_ sender: Any) {
+        createMan(name: "yoon", age: 29)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
     }
-
+    
     func testAPICall() {
         let url = URL(string: "https://us-central1-neartruck-79b87.cloudfunctions.net/api/getTest")!
         
@@ -28,11 +36,63 @@ class ViewController: UIViewController {
                 print("ERROR: ", error)
             } else if let data = data {
                 let str = String(data: data, encoding: .utf8)
-                print("RESULT: ", data)
+                print("RESULTTEST: ", str)
             }
         }
         
         task.resume()
     }
+    
+    func createMan(name: String, age: Int) {
+        let parameters: Parameters = ["name": name, "age": age]
+        let url = "https://us-central1-neartruck-79b87.cloudfunctions.net/api/menCreate"
+        
+        AF.request(url, method: .post, parameters: parameters, encoding: JSONEncoding.default).response { response in
+            if let error = response.error {
+                print("ERROR: ", error)
+            } else {
+                print("Created Man: \(String(data: response.data ?? Data(), encoding: .utf8) ?? "")")
+            }
+        }
+    }
+    
+    func readMen() {
+        let url = "https://us-central1-neartruck-79b87.cloudfunctions.net/api/menRead"
+        
+        AF.request(url, method: .get).response { response in
+            if let error = response.error {
+                print("ERROR: ", error)
+            } else {
+                print("Received Men: \(String(data: response.data ?? Data(), encoding: .utf8) ?? "")")
+            }
+        }
+    }
+    
+    func updateMan(name: String, age: Int) {
+        let parameters: [String: Any] = ["name": name, "age": age]
+        let url = "https://us-central1-neartruck-79b87.cloudfunctions.net/api/menUpdate"
+        
+        AF.request(url, method: .post, parameters: parameters, encoding: JSONEncoding.default).response { response in
+            if let error = response.error {
+                print("ERROR: ", error)
+            } else {
+                print("Updated Man: \(String(data: response.data ?? Data(), encoding: .utf8) ?? "")")
+            }
+        }
+    }
+    
+    func deleteMan(name: String) {
+        let parameters: [String: Any] = ["name": name]
+        let url = "https://us-central1-neartruck-79b87.cloudfunctions.net/api/men"
+        
+        AF.request(url, method: .post, parameters: parameters, encoding: JSONEncoding.default).response { response in
+            if let error = response.error {
+                print("ERROR: ", error)
+            } else {
+                print("Deleted Man: \(String(data: response.data ?? Data(), encoding: .utf8) ?? "")")
+            }
+        }
+    }
+    
 }
 
--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,8 @@
-const { spawn } = require('child_process');
-
+const admin = require('firebase-admin');
 const functions = require('firebase-functions');
 const express = require('express');
+
+admin.initializeApp();
 
 const app = express();
 
@@ -12,37 +13,88 @@ app.get('/getTest', (req, res) => {
     res.send('윤형찬 API 서버');
 });
 
-// app.post('/calc', (req, res) => {
-//     const num1 = req.body.num1;
-//     const num2 = req.body.num2;
-  
-//     if (!num1 || !num2) {
-//       res.status(400).send('파라미터가 정상적으로 수신되지 않음');
-//       return;
-//     }
+// Create (새로운 Man 추가)
+app.post('/menCreate', (req, res) => {
+    const { name, age } = req.body;
+    const ref = admin.database().ref('/men');
 
-//     const testProcess = spawn('python', ['./pyLogic/test.py', '3', '5']);
-//     let result = '';
-  
-//     testProcess.stdout.on('data', (data) => {
-//         result += data.toString();
-//     });
-  
-//     testProcess.stderr.on('data', (data) => {
-//       console.error(data.toString());
-//     });
-  
-//     testProcess.on('close', (code) => {
-//       if (code === 0) {
-//         const asd = String(result)
-//         const resultObject = { "결과": asd };
-//         const resultJSON = JSON.stringify(resultObject);
-//         res.send(resultJSON);
-//       } else {
-//         res.status(500).send('test.py 실행 실패');
-//       }
-//     });
-// });
+    ref.once('value').then(snapshot => {
+        let exists = false;
+
+        snapshot.forEach(childSnapshot => {
+            if (childSnapshot.val().name === name) {
+                exists = true;
+                return true;
+            }
+        });
+
+        if (!exists) {
+            ref.push({ name, age });
+            res.status(201).send({ success: true });
+        } else {
+            res.status(400).send({ success: false, message: 'Name already exists' });
+        }
+    }).catch(error => {
+        res.status(500).send('Error reading data');
+    });
+});
+
+// Read (모든 Man 읽기)
+app.get('/menRead', (req, res) => {
+    const ref = admin.database().ref('/men');
+    
+    ref.once('value').then(snapshot => {
+        const men = [];
+
+        snapshot.forEach(childSnapshot => {
+            men.push(childSnapshot.val());
+        });
+
+        res.send(men);
+    }).catch(error => {
+        res.status(500).send('Error reading data');
+    });
+});
+
+// Update (Man의 나이 업데이트)
+app.post('/men', (req, res) => {
+    const { name, age } = req.body;
+    const ref = admin.database().ref('/menUpdate');
+    
+    ref.once('value').then(snapshot => {
+        snapshot.forEach(childSnapshot => {
+            if (childSnapshot.val().name === name) {
+                childSnapshot.ref.update({ age });
+                res.send({ success: true });
+                return true;
+            }
+        });
+
+        res.status(404).send({ success: false, message: 'Name not found' });
+    }).catch(error => {
+        res.status(500).send('Error updating data');
+    });
+});
+
+// Delete (Man 삭제)
+app.post('/men', (req, res) => {
+    const { name } = req.body;
+    const ref = admin.database().ref('/men');
+    
+    ref.once('value').then(snapshot => {
+        snapshot.forEach(childSnapshot => {
+            if (childSnapshot.val().name === name) {
+                childSnapshot.ref.remove();
+                res.send({ success: true });
+                return true;
+            }
+        });
+
+        res.status(404).send({ success: false, message: 'Name not found' });
+    }).catch(error => {
+        res.status(500).send('Error deleting data');
+    });
+});
 
 app.use((req, res) => {
     res.status(404).send('올바른 요청 형식이 아닙니다.');
