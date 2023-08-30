77d73fb24c58b1e7577c6081e62bf4ec9ef7a781
윤형찬 <72960152+velmash@users.noreply.github.com>
2023-08-30 13:50:18 +0900

    :hammer: firebase functions API Call Test

--- NearTruck/NearTruck/ViewController.swift
+++ NearTruck/NearTruck/ViewController.swift
@@ -6,14 +6,33 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
-
+    
+    @IBAction func test(_ sender: Any) {
+        testAPICall()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
-
+    func testAPICall() {
+        let url = URL(string: "https://us-central1-neartruck-79b87.cloudfunctions.net/api/getTest")!
+        
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if let error = error {
+                print("ERROR: ", error)
+            } else if let data = data {
+                let str = String(data: data, encoding: .utf8)
+                print("RESULT: ", data)
+            }
+        }
+        
+        task.resume()
+    }
 }
 
