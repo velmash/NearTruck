41733f9d95186d7511659db71e2e144187e4f130
윤형찬 <72960152+velmash@users.noreply.github.com>
2023-08-30 13:27:03 +0900

    :hammer: firebase functions 테스트 코드 생성

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,51 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+const { spawn } = require('child_process');
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+const functions = require('firebase-functions');
+const express = require('express');
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+const app = express();
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/getTest', (req, res) => {
+    res.send('윤형찬 API 서버');
+});
+
+// app.post('/calc', (req, res) => {
+//     const num1 = req.body.num1;
+//     const num2 = req.body.num2;
+  
+//     if (!num1 || !num2) {
+//       res.status(400).send('파라미터가 정상적으로 수신되지 않음');
+//       return;
+//     }
+
+//     const testProcess = spawn('python', ['./pyLogic/test.py', '3', '5']);
+//     let result = '';
+  
+//     testProcess.stdout.on('data', (data) => {
+//         result += data.toString();
+//     });
+  
+//     testProcess.stderr.on('data', (data) => {
+//       console.error(data.toString());
+//     });
+  
+//     testProcess.on('close', (code) => {
+//       if (code === 0) {
+//         const asd = String(result)
+//         const resultObject = { "결과": asd };
+//         const resultJSON = JSON.stringify(resultObject);
+//         res.send(resultJSON);
+//       } else {
+//         res.status(500).send('test.py 실행 실패');
+//       }
+//     });
 // });
+
+app.use((req, res) => {
+    res.status(404).send('올바른 요청 형식이 아닙니다.');
+});
+
+exports.api = functions.https.onRequest(app);
